1378bf4f04550a0b8e24042abad7eb41da4bbd1a
Isaac To <candleindark@gmail.com>
2019-07-03 07:59:12 -0700

    Define interface for the find_inactive function

---
+++ rss_checker.py
@@ -0,0 +1,11 @@
+def find_inactive(rss_feed_urls, days_of_inactivity):
+    """
+    Given a dictionary with keys of company names (in str) and values of RSS feed urls (in str) to the corresponding
+    companies named. Obtain the list of companies, by name, that have no activity in their RSS feeds for a given number
+    of days.
+    :param rss_feed_urls: The given dictionary
+    :param days_of_inactivity: The given number of days of inactivity
+    :return: The list of companies, specified in name, that have no activity in their RSS feeds for a given number
+    of days.
+    """
+    pass
