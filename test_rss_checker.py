b3ec5cb25e9eac080d9b1698462d51b8a9011933
Isaac To <candleindark@gmail.com>
2019-07-03 12:15:50 -0700

    Add test case with dictionary only containing one feed

--- test_rss_checker.py
+++ test_rss_checker.py
@@ -15,6 +15,18 @@ class TestRssChecker(TestCase):
             self.assertEqual([], find_inactive(feed_urls_by_companies, 1))
             self.assertEqual([], find_inactive(feed_urls_by_companies, 99))
 
+        feed_urls_by_companies = {'bill_maher': 'http://billmaher.hbo.libsynpro.com/rss'}
+        with self.subTest(feed_urls_by_companies=feed_urls_by_companies):
+            # time stamp of the latest item
+            bill_maher_latest = calendar.timegm((2019, 7, 1, 20, 25, 4, 0, 182, 0))
+
+            # time elapsed since the latest item
+            time_elapsed_bill_maher = time.time() - bill_maher_latest
+
+            days_elapsed_bill_maher = time_elapsed_bill_maher // DAY_IN_SECONDS
+            self.assertEqual([], find_inactive(feed_urls_by_companies, days_elapsed_bill_maher + 1))
+            self.assertEqual(['bill_maher'], find_inactive(feed_urls_by_companies, days_elapsed_bill_maher - 1))
+
         feed_urls_by_companies = {'bill_maher': 'http://billmaher.hbo.libsynpro.com/rss',
                                   'bill_simmons': 'https://rss.art19.com/the-bill-simmons-podcast'}
 
